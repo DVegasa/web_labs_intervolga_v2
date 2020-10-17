ecb833c2ae78ae8ab362a2442acaf6ca66656891
Ed Khalturin <DracVeg@gmail.com>
2020-10-17 16:02:21 +0400

    Получаю теги

--- laba3.php
+++ laba3.php
@@ -3,7 +3,10 @@
     class TocConfig {
         public $name, $hLevel;
     }
-
+    
+    /**
+     * @return HTML вёрстка оглавления
+     */
     function getTocByHtml($html, $tocConfig) {
         $rawHtml = @file_get_contents("$html.html");  
         if (!empty($rawHtml)) {
@@ -13,15 +16,47 @@
         }
     }
 
-    class H {
-        public $level, $name;
-    }
-
+    $offset = 0;
     function getToc($html) {
         $headerPattern = "/<h([1-6]).?(.*?)<\/h.?>/";
-        preg_match_all($headerPattern, $html, $matches, PREG_OFFSET_CAPTURE);
-        print_r($matches);
-        print_r("\n\n\n");
-        print_r($html);
+        preg_match_all($headerPattern, $html, $matches, PREG_OFFSET_CAPTURE + PREG_SET_ORDER);
+
+        foreach ($matches as $hTag) {
+            $fullTag = $hTag[0][0];
+            $pos = $hTag[0][1];
+            $hLevel = $hTag[1][0];
+            
+            $hTagId;
+            if (hasHTagId($fullTag)) {
+                $hTagId = getHTagId($fullTag);
+            } else {
+                $hTagId = getGeneratedHTagId();
+                
+                global $offset;
+
+            }
+        }
+    }
+
+    function addToToc($id, $name, $hLevel) {
+
+    }
+
+    $generatedHTagIdPreffix = "tag";
+    function getGeneratedHTagId() {
+        static $idIndex = 1;
+        global $generatedHTagIdPreffix;
+        $id = $generatedHTagIdPreffix . $idIndex++;
+        return $id;
+    }
+
+    function hasHTagId($hTag) {
+        return getHTagId($hTag) === NULL ? 0 : 1;
+    }
+
+    function getHTagId($hTag) {
+        $hTagWithIdPattern = "/<h[1-6].*id=\"(\w*)\".*/";
+        preg_match($hTagWithIdPattern, $hTag, $matches);
+        return isset($matches[1]) ? $matches[1] : NULL;
     }
 ?>
