e1f54f1e1c498041821896fb4d10441bb2ec1e39
Ed Khalturin <DracVeg@gmail.com>
2020-10-21 20:34:29 +0400

    Что-то поменялось. Наверное, в лучшую сторону

--- toc.php
+++ toc.php
@@ -15,78 +15,83 @@
 
 
 <body>
-    <div> 
-        <h2>Анкета</h2>
+    <div class="begin">
+        <h2>Автогенератор оглавления</h2>
         <form enctype="multipart/form-data" method="POST">
             <p>Тип ввода<br> 
-                <input required type="radio" name="inputType" value="onSite" />Файл на сервере </br>
-                <input required type="radio" name="inputType" value="source" />Исходник </br>
-                <input required type="radio" name="inputType" value="htmlFile" />HTML файл </br>
+                <label> <input required type="radio" name="inputType" value="onSite" />Имя файла на сервере </label> </br>
+                <label> <input required type="radio" name="inputType" value="source" />Исходный код </label> </br>
+                <label> <input required type="radio" name="inputType" value="htmlFile" />Загрузка файла с ПК </label> </br>
 
-            <p>Введите имя файла на сервере, без расширения файла<br> 
+            <p>Имя файла на сервере, без расширения файла<br> 
             <input type="text" name="html" /></p>
 
-            <p>Вставьте Исходник<br> 
+            <p>Исходный код<br> 
             <input type="text" name="src" /></p>
 
-            <p>Загрузите файл .html<br> 
+            <p>Загрузка файла с ПК <br> 
             <input type="hidden" name="MAX_FILE_SIZE" value="1500000" />
             <input name="userfile" type="file" />
 
             <p>Максимальная вложенность: <br> 
-                <input required type="radio" name="hLevel" value="1" />h1 </br>
-                <input required type="radio" name="hLevel" value="2" />h2 </br>
-                <input required type="radio" name="hLevel" value="3" />h3 </br>
-                <input required type="radio" name="hLevel" value="4" />h4 </br>
-                <input required type="radio" name="hLevel" value="5" />h5 </br>
-                <input required checked type="radio" name="hLevel" value="6" />h6 </br>
+                <label> <input required type="radio" name="hLevel" value="1" />h1 </label> &nbsp;
+                <label> <input required type="radio" name="hLevel" value="2" />h2 </label> &nbsp;
+                <label> <input required type="radio" name="hLevel" value="3" />h3 </label> &nbsp;
+                <label> <input required type="radio" name="hLevel" value="4" />h4 </label> &nbsp;
+                <label> <input required type="radio" name="hLevel" value="5" />h5 </label> &nbsp;
+                <label> <input required checked type="radio" name="hLevel" value="6" />h6 </label> &nbsp;
 
             <p>Префикс для сгенерированных id. По умолчанию, toc<br> 
             <input required type="text" value="toc" name="idPreffix" /></p>
 
-            <input type="submit" value="Выполнить">
+            <input type="submit" class="btn btn-primary" value="Выполнить">
         </form>
-
+        &nbsp;
         <?php
-            if (!isset($_POST['hLevel'])) {
-                return;
-            }
-
-            require_once "laba3.php";
-            $tocConfig = new TocConfig;
-            $tocConfig->hLevel = $_POST['hLevel'];
-            if (!isset($_POST['idPreffix']) || $_POST['idPreffix'] === "") {
-                $_POST['idPreffix'] = 'toc';
-            }
-            $tocConfig->idPreffix = $_POST['idPreffix'];
-
-            $tocResult;
-            if ($_POST['inputType'] === 'source') {
-                $tocResult = getTocByHtmlSource($_POST['src'], $tocConfig);
-
-            } else if ($_POST['inputType'] === 'onSite'){
-                $tocConfig->name = $_POST['html'];
-                $tocResult = getTocByResourceName($_POST['html'], $tocConfig);
-
-            } else {
-                $tocResult = getTocByHtmlFile($_FILES['userfile'], $tocConfig);
-            }
-
-            
-            if ($tocResult instanceof TocResult) {
-                echo "Готовый TOC </br>";
-                echo htmlentities($tocResult->tocHtml);
-                echo "</br> </br> Обновлённый HTML </br> </br>";
-                echo htmlentities($tocResult->modifiedHtml);
-
-                echo "</br> </br> </br> </br>";
-                echo $tocResult->tocHtml;
-                echo $tocResult->modifiedHtml;
-                
-            } else if ($tocResult instanceof ErrorResult) {
-                echo "Ошибка: " . $tocResult->msg;
+            if (isset($_POST['hLevel'])) {
+                echo '<form method="POST">
+                <input class="btn btn-secondary" type="submit" value="Очистить"> </form>
+                </br>';
             }
         ?>
 
-    </div>
+        <?php
+            if (isset($_POST['hLevel'])) {
+                    
+                require_once "laba3.php";
+                $tocConfig = new TocConfig;
+                $tocConfig->hLevel = $_POST['hLevel'];
+                if (!isset($_POST['idPreffix']) || $_POST['idPreffix'] === "") {
+                    $_POST['idPreffix'] = 'toc';
+                }
+                $tocConfig->idPreffix = $_POST['idPreffix'];
+
+                $tocResult;
+                if ($_POST['inputType'] === 'source') {
+                    $tocResult = getTocByHtmlSource($_POST['src'], $tocConfig);
+
+                } else if ($_POST['inputType'] === 'onSite'){
+                    $tocConfig->name = $_POST['html'];
+                    $tocResult = getTocByResourceName($_POST['html'], $tocConfig);
+
+                } else {
+                    $tocResult = getTocByHtmlFile($_FILES['userfile'], $tocConfig);
+                }
+
+                
+                if ($tocResult instanceof TocResult) {
+                    echo "<h3> Как будет выглядеть оглавление </h3> ";
+                    echo $tocResult->tocHtml;
+
+                    echo "</br></br> <h3> HTML-код оглавления </br> </h3>";
+                    echo htmlentities($tocResult->tocHtml);
+
+                    echo "</br></br> <h3> Модифицированный HTML </h3> ";
+                    echo htmlentities($tocResult->modifiedHtml);
+                    
+                } else if ($tocResult instanceof ErrorResult) {
+                    echo "Ошибка: " . $tocResult->msg;
+                }
+            }
+        ?>
 </body>
