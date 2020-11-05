557ce082f9100483bc034a1711a7b06c59190ccb
Ed Khalturin <DracVeg@gmail.com>
2020-11-05 09:48:35 +0400

    formatting

--- account.php
+++ account.php
@@ -1,142 +1,174 @@
 <!DOCTYPE HTML>
 <html>
 
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>volsu_bootstrapp</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/css/swiper.min.css">
-    <link rel="stylesheet" href="assets/css/Navigation-with-Button.css">
-    <link rel="stylesheet" href="assets/css/Simple-Slider.css">
-    <link rel="stylesheet" href="assets/css/styles.css">
-	<style>
-   body {
-    background-image: url(assets/img/fon_akk.jpg); 
-   }
-   </style>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+  <title>volsu_bootstrapp</title>
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/css/swiper.min.css">
+  <link rel="stylesheet" href="assets/css/Navigation-with-Button.css">
+  <link rel="stylesheet" href="assets/css/Simple-Slider.css">
+  <link rel="stylesheet" href="assets/css/styles.css">
+  <style>
+    body {
+      background-image: url(assets/img/fon_akk.jpg);
+    }
+  </style>
 </head>
+
 <body>
 
-        <?php
-                    session_start();
-                    if (!isset($_SESSION['username'])) {
-                        header('Location: log-in.php');
-                    }
-                ?>
+  <?php
+  session_start();
+  if (!isset($_SESSION['username'])) {
+    header('Location: log-in.php');
+  }
+  ?>
 
-<nav class="navbar navbar-light navbar-expand-md navigation-clean-button" style="background: rgb(103,16,94);max-height: 59px;">
-        <div class="container"><a class="navbar-brand" href="index.php" style="color: rgb(255,255,255);">MoneyBro</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navcol-1">
-                <ul class="nav navbar-nav mr-auto">
-                    <li class="nav-item"><a class="nav-link" href="exchanges.php" style="color: rgb(255,255,255);margin-left: 0px;">Текущий курс</a></li>
-                    <li class="nav-item"><a class="nav-link text-white" href="blog.php" style="color: rgb(255,255,255);">Блог</a></li>
-					</ul></div>
-                </ul></div>
-        </div>
-    </nav>
-
-    <?php 
-          $servername = "localhost";
-          $username = "root";
-          $password = "";
-          $mysqli = new mysqli($servername, $username, $password, "master");
-          $uname = $_SESSION['username'];
-    
-          $result = $mysqli->query("SELECT * FROM `users`
-                                    WHERE `username` = '$uname'");
-    
-          $row = $result->fetch_assoc();
-        ?>
-   
-   <div class="cabinet">
-     <div class="cabinet_img">
-    <img src="
-    <?php echo $row['ava_url']; ?>
-    " width="180px">
-	 </div>
-	 <div class="cabinet_info">
-	   <p class="name"> <strong><?php echo $row['first_name']; ?> </strong> </p>
-	   <p class="name"> <strong><?php echo $row['last_name']; ?> </strong> </p>
-	   <p class="LONE"> Секретный ключ: <?php echo $row['secret']; ?>  </p>
-	   <p class="LTWO"> Роль в системе: <?php echo $row['role']; ?> </p> 
-   </div>
-      
-                  </br>
-      <div class="form-item">
-        <form method="POST" action="logout.php" class="reg-form">
-          <button type="submit" class="reg-form-item au-but">Выйти из аккаунта        </button>
-
-        </form>
+  <nav class="navbar navbar-light navbar-expand-md navigation-clean-button" style="background: rgb(103,16,94);max-height: 59px;">
+    <div class="container"><a class="navbar-brand" href="index.php" style="color: rgb(255,255,255);">MoneyBro</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+      <div class="collapse navbar-collapse" id="navcol-1">
+        <ul class="nav navbar-nav mr-auto">
+          <li class="nav-item"><a class="nav-link" href="exchanges.php" style="color: rgb(255,255,255);margin-left: 0px;">Текущий курс</a></li>
+          <li class="nav-item"><a class="nav-link text-white" href="blog.php" style="color: rgb(255,255,255);">Блог</a></li>
+        </ul>
       </div>
+      </ul>
+    </div>
+    </div>
+  </nav>
 
-   
-   
-<p>&nbsp;</p>
-  <form id="form_viz" name="form1" method="post">
-    <table id="Table1">
-<tr>
-<td class="cell0"><p >День Визита</p></td>
-<td class="cell0"><p >Месяц</p></td>
-<td class="cell0"><p >Время</p></td>
-<td class="cell0"><p >Сумма обмена</p></td>
-<td class="cell0"><p >Валюта</p></td>
-<td class="cell0"><p >Сумма получения</p></td>
-<td class="cell0"><p >Валюта</p></td>
-<td class="cell0"><p >Статус</p></td>
-</tr>
-<tr>
-<td class="cell0"><p >&nbsp;</p></td>
-<td class="cell0"><p >&nbsp;</p></td>
-<td class="cell0"><p >&nbsp;</p></td>
-<td class="cell0"><p >&nbsp;</p></td>
-<td class="cell0"><p >&nbsp;</p></td>
-<td class="cell0"><p >&nbsp;</p></td>
-<td class="cell0"><p >&nbsp;</p></td>
-<td class="cell0"><p >&nbsp;</p></td>
-</tr>
-</table>
+  <?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $mysqli = new mysqli($servername, $username, $password, "master");
+  $uname = $_SESSION['username'];
+
+  $result = $mysqli->query("SELECT * FROM `users` WHERE `username` = '$uname'");
+
+  $row = $result->fetch_assoc();
+  ?>
+
+  <div class="cabinet">
+    <div class="cabinet_img">
+      <img src="<?php echo $row['ava_url']; ?>" width="180px">
+    </div>
+    <div class="cabinet_info">
+      <p class="name"> <strong><?php echo $row['first_name']; ?> </strong> </p>
+      <p class="name"> <strong><?php echo $row['last_name']; ?> </strong> </p>
+      <p class="LONE"> Секретный ключ: <?php echo $row['secret']; ?> </p>
+      <p class="LTWO"> Роль в системе: <?php echo $row['role']; ?> </p>
+    </div>
+
+    </br>
+    <div class="form-item">
+      <form method="POST" action="logout.php" class="reg-form">
+        <button type="submit" class="reg-form-item au-but">Выйти из аккаунта </button>
+
+      </form>
+    </div>
+
+
+
+    <p>&nbsp;</p>
+    <form id="form_viz" name="form1" method="post">
+      <table id="Table1">
+        <tr>
+          <td class="cell0">
+            <p>День Визита</p>
+          </td>
+          <td class="cell0">
+            <p>Месяц</p>
+          </td>
+          <td class="cell0">
+            <p>Время</p>
+          </td>
+          <td class="cell0">
+            <p>Сумма обмена</p>
+          </td>
+          <td class="cell0">
+            <p>Валюта</p>
+          </td>
+          <td class="cell0">
+            <p>Сумма получения</p>
+          </td>
+          <td class="cell0">
+            <p>Валюта</p>
+          </td>
+          <td class="cell0">
+            <p>Статус</p>
+          </td>
+        </tr>
+        <tr>
+          <td class="cell0">
+            <p>&nbsp;</p>
+          </td>
+          <td class="cell0">
+            <p>&nbsp;</p>
+          </td>
+          <td class="cell0">
+            <p>&nbsp;</p>
+          </td>
+          <td class="cell0">
+            <p>&nbsp;</p>
+          </td>
+          <td class="cell0">
+            <p>&nbsp;</p>
+          </td>
+          <td class="cell0">
+            <p>&nbsp;</p>
+          </td>
+          <td class="cell0">
+            <p>&nbsp;</p>
+          </td>
+          <td class="cell0">
+            <p>&nbsp;</p>
+          </td>
+        </tr>
+      </table>
+  </div>
+
+  <br>
+  <div id="form0">
+    <h1>Запланируйте визит</h1>
+    <br>
+    <label>Веберите дату визита</label>
+    <input type="date" required>
+    <br>
+    <label>Веберите время визита</label>
+    <input type="time" required>
+    <br>
+    <br>
+    <label>Введите сумму для перевода и валюту конвертации
+      (точный курс будет известен во время визита)</label>
+    <br>
+    <input type="number" required>
+
+    <select name="curFrom" size="1">
+      <option value="">USD</option>
+      <option>EUR</option>
+      <option>GBP</option>
+      <option>JPY</option>
+      <option>CHF</option>
+      <option>RUB</option>
+    </select>
+    <label>--></label>
+    <select name="curTo" size="1">
+      <option value="">USD</option>
+      <option>EUR</option>
+      <option>GBP</option>
+      <option>JPY</option>
+      <option>CHF</option>
+      <option>RUB</option>
+    </select>
+    <br>
+    <br>
+    <button id="bt">Запланировать</button>
+  </div>
+
   </form>
-</div>
-<br>
-<div id="form0">
-<h1>Запланируйте визит</h1>
-	<br>
-<label>Веберите дату визита</label>
-<input type="date" required>
-<br>
-<label>Веберите время визита</label>
-<input type="time" required>
-<br>
-	<br>
-<label class="textic">Введите сумму для перевода</label>
-<br>
-	<br>
-<input type="number" required>
-
-<select name="Combobox" size="1" >
-	<option value="">USD</option>
-	<option>EUR</option>
-	<option>GBP</option>
-	<option>JPY</option>
-	<option>CHF</option>
-	<option>RUB</option>
-</select>
-	<label>-------></label>
-<label>Здесь будет число</label>
-<select name="Combobox" size="1" >
-	<option value="">USD</option>
-	<option>EUR</option>
-	<option>GBP</option>
-	<option>JPY</option>
-	<option>CHF</option>
-	<option>RUB</option>
-</select>
-<br>
-<br>
-<button id="bt">Запланировать</button>
-</div>
-
 
 
 
