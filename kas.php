add7d16d459f7fbc65fe8c1e3d48fe7d2d2ee6bd
Gulushka <andre454545@mail.ru>
2020-12-01 22:22:54 +0400

    Create kas.php

---
+++ kas.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+  <title>volsu_bootstrapp</title>
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/css/swiper.min.css">
+  <link rel="stylesheet" href="assets/css/Navigation-with-Button.css">
+  <link rel="stylesheet" href="assets/css/Simple-Slider.css">
+  <link rel="stylesheet" href="assets/css/styles.css">
+	<style>
+    body {
+      background-image: url(assets/img/fon_akk.jpg);
+    }
+  </style>
+</head>
+<body>
+<nav class="navbar navbar-light navbar-expand-md navigation-clean-button" style="background: rgb(103,16,94);max-height: 59px;">
+    <div class="container"><a class="navbar-brand" href="index.php" style="color: rgb(255,255,255);">MoneyBro</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+      <div class="collapse navbar-collapse" id="navcol-1">
+        <ul class="nav navbar-nav mr-auto">
+          <li class="nav-item"><a class="nav-link" href="exchanges.php" style="color: rgb(255,255,255);margin-left: 0px;">Текущий курс</a></li>
+          <li class="nav-item"><a class="nav-link text-white" href="blog.php" style="color: rgb(255,255,255);">Блог</a></li>
+        </ul>
+      </div>
+      </ul>
+    </div>
+    </div>
+  </nav>
+
+<div id="kas">
+<img id="imgc" src="assets/img/avatar.jpg " width="180px">
+<h2 align="center"><b><u>Фамилия Имя</u></b></h2>
+<br>
+<h5 align="center">Личный номер</h5>
+<br>
+<h4 align="center">Стаж работы</h4>
+<br>
+<h4 align="center">График работы</h4>
+
+</div>
+
+
+</body>
+</html>
